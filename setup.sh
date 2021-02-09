0b3c28571eccab00eece9844f97e60c0820758f7
Guillaume Dupont <gdupont@student.42.fr>
2021-02-09 10:53:26 +0100

    wip

--- setup.sh
+++ setup.sh
@@ -6,29 +6,35 @@
 #20GB of free disk space
 #Internet connection
 
+# if there are issues with permissions do the following command
+#sudo usermod -aG docker user42; newgrp docker
+
 # If we want to launch minikube with --driver=none we must install conntrack
-if ! which conntrack &>/dev/null; then
-	sudo apt-get install -y conntrack
-fi
+# if ! which conntrack &>/dev/null; then
+# 	sudo apt-get install -y conntrack
+# fi
+
+# We delete previous minikube 
+minikube delete
 
 # Stopping nginx is necessary to avoid potential future issues
-sudo service nginx stop
+service nginx stop
 
 # sudo systemctl enable docker.service
 
 # Since we are on a VM we launch minikube with no driver selected
 echo "Launching minikube..."
-sudo minikube start --driver=none
+minikube start --vm-driver=docker
 
 ##LoadBalancer MetalLB##
 echo "Installing MetalLB..."
 
-sudo kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-sudo kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 # On first install only
-sudo kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
-sudo kubectl apply -f srcs/Metallb/metallb_config.yaml
+kubectl apply -f srcs/Metallb/metallb_config.yaml
 
 
 
