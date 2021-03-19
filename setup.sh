9a66715dea6ebd1b4b55b0aa406965f17c128c99
gdupont <gdupont@student.42.fr>
2021-03-19 19:24:00 +0100

    cleaned

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 # if there are issues with permissions do the following command
 #sudo usermod -aG docker user42; newgrp docker
 
-
 minikube delete
 minikube start --vm-driver=docker
 clear
 #MAKING SURE THE DOCKER IMAGES WILL BE IN THE MINIKUBE ENVIRONMENT
 eval $(minikube docker-env)
-
-
 
 ##LoadBalancer MetalLB##
 echo "Installing MetalLB..."
@@ -18,14 +15,13 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f srcs/Metallb/metallb_config.yaml
 
-
 # CREATING IMAGES
-# docker build -t my_nginx srcs/nginx
-# docker build -t my_wordpress srcs/wordpress
-# docker build -t my_phpmyadmin srcs/phpmyadmin
-# docker build -t my_mysql srcs/mysql
-# docker build -t my_ftps srcs/ftps
-# docker build -t my_influxdb srcs/influxdb
+docker build -t my_nginx srcs/nginx
+docker build -t my_wordpress srcs/wordpress
+docker build -t my_phpmyadmin srcs/phpmyadmin
+docker build -t my_mysql srcs/mysql
+docker build -t my_ftps srcs/ftps
+docker build -t my_influxdb srcs/influxdb
 docker build -t my_grafana srcs/grafana
 
 ##Persistent Volumes##
@@ -47,7 +43,7 @@ kubectl apply -f srcs/mysql/mysql.yaml
 ##FTPS##
 kubectl apply -f srcs/ftps/ftps.yaml
 
-##INFLUXDB + TELEGRAF##
+##INFLUXDB##
 kubectl apply -f srcs/influxdb/influxdb.yaml
 
 ##GRAFANA##
